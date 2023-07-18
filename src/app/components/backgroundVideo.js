cf590eb0cd558f6273db2eeb73bf4e3e605c1045
Arief Maulana <ariefmaulana1303@gmail.com>
2023-07-18 12:48:15 +0800

    update backgroundVideo

--- src/app/components/backgroundVideo.js
+++ src/app/components/backgroundVideo.js
@@ -4,7 +4,7 @@ export default function BackgroundVideo() {
 	return (
 		<div className='video-background'>
 			<video autoPlay loop muted playsInline>
-				<source src={dataVideo.list[0].tautan} type='video/mp4' />
+				<source src={dataVideo.list[1].tautan} type='video/mp4' />
 			</video>
 		</div>
 	)
